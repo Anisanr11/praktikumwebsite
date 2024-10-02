297c1e733a39a47b878a54d384e07f7aa9a2d9b4
Anisanr11 <nurohmaanisa271@gmail.com>
2024-10-02 14:08:20 +0700

    Create isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeIn("<center>");
+document.writeIn("<hr width=600 color=black>");
+document.writeIn("<H1>UNIVERSITAS LAMPUNG</H1>");
+document.writeIn("<H2>PENDIDIKAN TEKNOLOGI INFORMASI</H2>");
+document.writeIn("<H3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</H3>");
+document.writeIn("<HR WIDTH=600 COLOR=BLACK>");
+document.writeIn("</center>");
